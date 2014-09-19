fb515015a431c0b5a9971375065750c0516cea13
sl1k <sl1k@meta.ua>
2014-09-19 16:45:37 +0300

    changed https url

--- server/public/extension/broadcaster.js
+++ server/public/extension/broadcaster.js
@@ -98,7 +98,7 @@ vkPlayer = function() {
 
 var  Broadcaster = function() {
 
-  var url = 'https://radi0.me';
+  var url = '//radi0.me';
   var socket;
   var bEvents = [];
   var connected = false;
--- server/public/js/main.js
+++ server/public/js/main.js
@@ -132,7 +132,7 @@ html5Player = function() {
 //* Listener Class
 
 var Listener = function() {
-  var url = 'http://radi0.me';
+  var url = '//radi0.me';
   var socket;
   var events = [];
   var uid = Math.round(Math.random() * 1000000);
